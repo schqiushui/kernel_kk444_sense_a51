c32b17c9c53a4f6c65a4c2be31d7c2f904987d3c
flar2 <asegaert@gmail.com>
2014-10-27 00:30:36 -0400

    allow stock modules to load
    Conflicts:
    	kernel/module.c

--- kernel/module.c
+++ kernel/module.c
@@ -1086,7 +1086,15 @@ static int check_version(Elf_Shdr *sechdrs,
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
 
-	
+	if(!strncmp("wlan", mod->name, 4))
+		return 1;
+
+	if(!strncmp("moc_", mod->name, 4))
+		return 1;
+
+	if(!strncmp("texfat", mod->name, 6))
+		return 1;
+
 	if (!crc)
 		return 1;
 
